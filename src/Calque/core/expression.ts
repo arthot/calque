8e1c3d5654546bcaa88c35a82920565612b21345
arthot <artem-ktf@hotmail.com>
2015-12-02 00:34:47 +0300

    Added base expression class

---
+++ src/Calque/core/expression.ts
@@ -0,0 +1,11 @@
+﻿import utils = require('utils');
+
+export class Expression {
+    code: string;
+    scopeInput: any;
+    scopeOutput: any;
+
+    constructor(code: string, scope: any) {
+        
+    }
+}
