7808effd38190416ed7484e91e972afa5cac14dc
arthot <artem-ktf@hotmail.com>
2015-12-02 23:41:13 +0300

    Setting button toggle dark/light theme

--- src/Calque/core/app.ts
+++ src/Calque/core/app.ts
@@ -1,12 +1,15 @@
 ﻿import HelpPage = require('help');
 import Workspace = require('workspace');
+import Settings = require('settings');
 
 class Application {
     Help = new HelpPage();
     Workspace: Workspace;
+    Settings: Settings;
 
     constructor(inputEl: HTMLInputElement, outputEl: HTMLElement) {
         this.Workspace = new Workspace(inputEl, outputEl);
+        this.Settings = new Settings();
     }
 }
 
