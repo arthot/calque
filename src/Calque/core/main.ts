fca96d1e18262a2508ede544eccba33e003c49ba
arthot <artem-ktf@hotmail.com>
2015-12-04 10:33:45 +0300

    Basic localization

--- src/Calque/core/main.ts
+++ src/Calque/core/main.ts
@@ -39,6 +39,10 @@
         }
     };
 
+    app.onloaded = function () {
+        WinJS.Resources.processAll();
+    }
+
     app.oncheckpoint = function (args) {
         // TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
         // You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
