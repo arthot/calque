86e805768e53288e0e18139f1cf1b9cea892c4a6
arthot <artem-ktf@hotmail.com>
2015-12-07 10:13:33 +0300

    Handing suspend, restore, exit

--- src/Calque/core/main.ts
+++ src/Calque/core/main.ts
@@ -6,13 +6,6 @@
 
     app.onactivated = function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
-            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                // TODO: This application has been newly launched. Initialize your application here.
-            } else {
-                // TODO: This application was suspended and then terminated.
-                // To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
-            }
-
             require.config({
                 baseUrl: 'core',
                 paths: {
@@ -20,11 +13,11 @@
                 }
             });
 
-            require(['app'], function (App) {
+            require(['app'], function (app) {
                 var input = <HTMLInputElement>document.getElementById('input');
                 var output = document.getElementById('output');
 
-                var app = new App(input, output);
+                app.init(input, output);
 
                 ko.components.register("help", {
                     viewModel: { instance: app.help },
@@ -35,6 +28,12 @@
                 ko.applyBindings(app);
 
                 input.select();
+
+                if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                    app.restore(WinJS.Application.local);
+                } else {
+                    app.restore(WinJS.Application.local, WinJS.Application.sessionState.value);
+                }
             });
         }
     };
@@ -44,9 +43,9 @@
     }
 
     app.oncheckpoint = function (args) {
-        // TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
-        // You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
-        // If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+        require(['app'], function (app) {
+            WinJS.Application.sessionState.value = app.workspace.inputEl.value;
+        });
     };
 
     app.start();
