4c50172f81f16289ab4c61ba3ff3c84377fa2780
arthot <artem-ktf@hotmail.com>
2015-12-07 10:12:16 +0300

    Make App singelton & added restore/save functions

--- src/Calque/core/app.ts
+++ src/Calque/core/app.ts
@@ -5,12 +5,27 @@ import Settings = require('settings');
 class Application {
     help = new HelpPage();
     workspace: Workspace;
-    settings: Settings;
+    settings: Settings.Settings;
 
-    constructor(inputEl: HTMLInputElement, outputEl: HTMLElement) {
+    init(inputEl: HTMLInputElement, outputEl: HTMLElement) {
         this.workspace = new Workspace(inputEl, outputEl);
-        this.settings = new Settings();
+        this.settings = new Settings.Settings();
+
+        this.help.isVisible.subscribe(v => this.save());
+        this.settings.theme.subscribe(v => this.save());
+    }
+
+    restore(data: IOHelper, value: string) {
+        if (value)
+            this.workspace.inputEl.value = value;
+        data.readText('help').done(v => v ? this.help.isVisible(v === 'true') : true);
+        data.readText('theme').done(v => this.settings.theme(Settings.Theme[v]));
+    }
+
+    save() {
+        WinJS.Application.local.writeText('help', String(this.help.isVisible()));
+        WinJS.Application.local.writeText('theme', Settings.Theme[this.settings.theme()]);
     }
 }
 
-export = Application;
+export = new Application();
