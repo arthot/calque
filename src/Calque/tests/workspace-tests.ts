70560cfe2d1cd3af3deb243eae212647955fc024
arthot <artem-ktf@hotmail.com>
2015-12-08 09:49:10 +0300

    Mocha ts boilerplate packages & files

---
+++ src/Calque/tests/workspace-tests.ts
@@ -0,0 +1,11 @@
+﻿import assert = require('power-assert')
+import Workspace = require('../core/Workspace')
+
+describe("Workspace", () => {
+    context("input()", () => {
+
+        it("should split to lines", () => {
+
+        });
+    });
+});
