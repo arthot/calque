62015fbd40cc15e8da471951854b79a525faf722
arthot <artem-ktf@hotmail.com>
2015-12-09 22:37:18 +0300

    Cover all help examples with workspace tests

--- src/Calque/test/workspace-tests.ts
+++ src/Calque/test/workspace-tests.ts
@@ -1,12 +1,59 @@
 ﻿import Workspace = require('../core/Workspace');
 
 describe("Workspace", () => {
-    context("input = 1\r\n\2", () => {
+    context("1; 2", () => {
         it("should split to 2 lines", () => {
             var workspace = new Workspace();
-            workspace.input("1\r\n\2");
-
+            workspace.input("1 \n 2");
             chai.assert.equal(workspace.lines.length, 2);
+        });
+    });
+
+    context("sqrt(3^2 + 4^2)", () => {
+        it(" = 5", () => {
+            var workspace = new Workspace();
+            workspace.input("sqrt(3^2 + 4^2)");
+            chai.assert.equal(workspace.expressions[0].result, '5');
+        });
+    });
+
+    context("a=1; a+1", () => {
+        it(" = 2", () => {
+            var workspace = new Workspace();
+            workspace.input("a=1 \n a+1");
+            chai.assert.equal(workspace.expressions[1].result, '2');
+        });
+    });
+
+    context("a=1; b=a+1; last+1", () => {
+        it(" = 4", () => {
+            var workspace = new Workspace();
+            workspace.input("a=1 \n b=a+1 \n last+2");
+            chai.assert.equal(workspace.expressions[2].result, '4');
+        });
+    });
+
+    context("a = 2 * 2 +2 # And this", () => {
+        it(" = 6", () => {
+            var workspace = new Workspace();
+            workspace.input("a = 2 * 2 + 2 # And this = 6");
+            chai.assert.equal(workspace.expressions[0].result, '6');
+        });
+    });
+
+    context("pow2(x) = x ^ 2; pow2(6)", () => {
+        it(" = 36", () => {
+            var workspace = new Workspace();
+            workspace.input("pow2(x) = x ^ 2 \n pow2(6)");
+            chai.assert.equal(workspace.expressions[1].result, '36');
+        });
+    });
+
+    context("input a=1;a+1", () => {
+        it("should generate file: a=1 = 1; a+1 = 2", () => {
+            var workspace = new Workspace();
+            workspace.input("a=1 \n a+1");
+            chai.assert.equal(workspace.generateText(), 'a=1  = 1\r\n a+1 = 2\r\n');
         });
     });
 });
