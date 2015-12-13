94339b515d4cdef1c8519940ee50e9c0cc4abcbf
arthot <artem-ktf@hotmail.com>
2015-12-14 00:35:48 +0300

    Added gulp-bom package & task to add bom to *.css/*.js files

--- src/Calque/gulpfile.js
+++ src/Calque/gulpfile.js
@@ -1,9 +1,17 @@
 /// <binding BeforeBuild='less' />
 var gulp = require('gulp'),
+    bom = require('gulp-bom'),
     less = require('gulp-less');
 
 gulp.task('less', function () {
     return gulp.src('./styles/**/*.less')
       .pipe(less())
+      .pipe(bom())
       .pipe(gulp.dest('./styles/'));
+});
+
+gulp.task('bom', function () {
+    return gulp.src(['./bower_components/**/*.css', './bower_components/**/*.js'])
+      .pipe(bom())
+      .pipe(gulp.dest('./bower_components/'));
 });
